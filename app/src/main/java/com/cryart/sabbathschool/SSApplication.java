568b803d4ff1581d046fd5c4fc6bc857aa51fdbf
VitalikL <lim.vitaliy@gmail.com>
2016-10-16 02:41:56 -0400

    Add SSBaseActivity for Drawer Remove unnecessary dependencies Add Google Material Design Icons dependency Add vector to SSSPlashActivity and SSLoginActivity - fall back to @mipmap/ic_launcher for api < 21

--- app/src/main/java/com/cryart/sabbathschool/SSApplication.java
+++ app/src/main/java/com/cryart/sabbathschool/SSApplication.java
@@ -24,9 +24,17 @@ package com.cryart.sabbathschool;
 
 import android.app.Application;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.crashlytics.android.Crashlytics;
 import com.cryart.sabbathschool.api.SSApiService;
+import com.mikepenz.iconics.IconicsDrawable;
+import com.mikepenz.materialdrawer.util.AbstractDrawerImageLoader;
+import com.mikepenz.materialdrawer.util.DrawerImageLoader;
+import com.mikepenz.materialdrawer.util.DrawerUIUtils;
 
 import io.fabric.sdk.android.Fabric;
 import rx.Scheduler;
@@ -51,6 +59,31 @@ public class SSApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Fabric.with(this, new Crashlytics());
+
+        DrawerImageLoader.init(new AbstractDrawerImageLoader() {
+            @Override
+            public void set(ImageView imageView, Uri uri, Drawable placeholder) {
+                Glide.with(imageView.getContext()).load(uri).placeholder(placeholder).into(imageView);
+            }
+
+            @Override
+            public void cancel(ImageView imageView) {
+                Glide.clear(imageView);
+            }
+
+            @Override
+            public Drawable placeholder(Context ctx, String tag) {
+                if (DrawerImageLoader.Tags.PROFILE.name().equals(tag)) {
+                    return DrawerUIUtils.getPlaceHolder(ctx);
+                } else if (DrawerImageLoader.Tags.ACCOUNT_HEADER.name().equals(tag)) {
+                    return new IconicsDrawable(ctx).iconText(" ").backgroundColorRes(com.mikepenz.materialdrawer.R.color.primary).sizeDp(56);
+                } else if ("customUrlItem".equals(tag)) {
+                    return new IconicsDrawable(ctx).iconText(" ").backgroundColorRes(R.color.md_red_500).sizeDp(56);
+                }
+
+                return super.placeholder(ctx, tag);
+            }
+        });
     }
 
     public Scheduler defaultSubscribeScheduler() {
