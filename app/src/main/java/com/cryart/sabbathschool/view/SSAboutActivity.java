5c8b1a3b5375a3624983b7b2ab24289c3ca784dd
VitalikL <lim.vitaliy@gmail.com>
2016-10-22 21:25:27 -0400

    Add About Activity

---
+++ app/src/main/java/com/cryart/sabbathschool/view/SSAboutActivity.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2016 Adventech <info@adventech.io>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.cryart.sabbathschool.view;
+
+
+import android.databinding.DataBindingUtil;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cryart.sabbathschool.R;
+import com.cryart.sabbathschool.databinding.SsAboutActivityBinding;
+
+public class SSAboutActivity extends AppCompatActivity {
+    private SsAboutActivityBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.ss_about_activity);
+        setSupportActionBar(binding.ssAppBar.ssToolbar);
+        ActionBar ssToolbar = getSupportActionBar();
+        if (ssToolbar != null) {
+            ssToolbar.setDisplayShowCustomEnabled(true);
+            ssToolbar.setDisplayShowTitleEnabled(false);
+            ssToolbar.setDisplayHomeAsUpEnabled(true);
+        }
+        binding.ssAppBar.toolbarTitle.setText("About");
+        binding.ssAppBar.toolbarTitle.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/PTF76F.ttf"));
+    }
+}
