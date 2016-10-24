333e2137da1ac3ba8aca5fd61baf971975e1a7fb
VitalikL <lim.vitaliy@gmail.com>
2016-10-23 21:22:56 -0400

    Smarter app open logic

--- app/src/main/java/com/cryart/sabbathschool/viewmodel/SSLoginViewModel.java
+++ app/src/main/java/com/cryart/sabbathschool/viewmodel/SSLoginViewModel.java
@@ -24,8 +24,10 @@ package com.cryart.sabbathschool.viewmodel;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.databinding.ObservableInt;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.Toast;
@@ -35,6 +37,7 @@ import com.cryart.sabbathschool.R;
 import com.cryart.sabbathschool.misc.SSConstants;
 import com.cryart.sabbathschool.misc.SSUserManager;
 import com.cryart.sabbathschool.model.SSUser;
+import com.cryart.sabbathschool.view.SSLessonsActivity;
 import com.cryart.sabbathschool.view.SSLoginActivity;
 import com.cryart.sabbathschool.view.SSQuarterliesActivity;
 import com.facebook.AccessToken;
@@ -154,9 +157,20 @@ public class SSLoginViewModel implements SSViewModel, FirebaseAuth.AuthStateList
     }
 
     private void openApp(){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+        String lastQuarterlyIndex = prefs.getString(SSConstants.SS_LAST_QUARTERLY_INDEX, "");
+
         Intent launchNextActivity;
-        launchNextActivity = new Intent(context, SSQuarterliesActivity.class);
-        launchNextActivity.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        if (false){
+            launchNextActivity = new Intent(context, SSLessonsActivity.class);
+            launchNextActivity.putExtra(SSConstants.SS_QUARTERLY_INDEX_EXTRA, lastQuarterlyIndex);
+        } else {
+            launchNextActivity = new Intent(context, SSQuarterliesActivity.class);
+            launchNextActivity.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        }
+
         context.startActivity(launchNextActivity);
     }
 
