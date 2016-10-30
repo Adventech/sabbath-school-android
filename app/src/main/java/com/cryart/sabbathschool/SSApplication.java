b344d62cebe46f86170e7a4f9555357ad93d5e4f
VitalikL <lim.vitaliy@gmail.com>
2016-10-30 13:22:19 -0400

    Make sure Drawer header image can be loaded via URL

--- app/src/main/java/com/cryart/sabbathschool/SSApplication.java
+++ app/src/main/java/com/cryart/sabbathschool/SSApplication.java
@@ -24,9 +24,14 @@ package com.cryart.sabbathschool;
 
 import android.app.Application;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.widget.ImageView;
 
 import com.crashlytics.android.Crashlytics;
 import com.google.firebase.database.FirebaseDatabase;
+import com.mikepenz.materialdrawer.util.AbstractDrawerImageLoader;
+import com.mikepenz.materialdrawer.util.DrawerImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
@@ -59,6 +64,18 @@ public class SSApplication extends Application {
 
         ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this).build();
         ImageLoader.getInstance().init(config);
+
+        DrawerImageLoader.init(new AbstractDrawerImageLoader() {
+            @Override
+            public void set(ImageView imageView, Uri uri, Drawable placeholder) {
+                ImageLoader.getInstance().displayImage(uri.toString(), imageView);
+            }
+
+            @Override
+            public void cancel(ImageView imageView) {
+                ImageLoader.getInstance().cancelDisplayTask(imageView);
+            }
+        });
     }
 
     public Scheduler defaultSubscribeScheduler() {
