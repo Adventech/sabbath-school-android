f3702430372015ce867084bcf9735ff30cecb03c
VitalikL <lim.vitaliy@gmail.com>
2018-04-16 21:43:57 -0400

    Adding rev 29 texts Adding new screenshots Adding FrameIt implementation in revision script

--- fastlane/create_changelog.sh
+++ fastlane/create_changelog.sh
@@ -5,3 +5,7 @@ for dir in metadata/android/*/changelogs; do touch -- "$dir/27.txt"; done;
 ///
 
 find . -name "*.png" -exec convert google_pixel_frame.png '{}' -gravity center -geometry -6-22 -composite '{}' \;
+
+export PATH=$PATH:/Users/vitaliy/Library/Android/sdk/platform-tools
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
--- fastlane/revision.sh
+++ fastlane/revision.sh
@@ -11,6 +11,10 @@ case $i in
     TRANSLATE="${i#*=}"
     shift
     ;;
+    -f=*|--frameit=*)
+    FRAMEIT="${i#*=}"
+    shift
+    ;;
     --default)
     DEFAULT=YES
     shift
@@ -41,5 +45,53 @@ if [[ $TRANSLATE ]]; then
                 ./trans -b -i metadata/android/en-US/changelogs/$TRANSLATE.txt -o $dir/changelogs/$TRANSLATE.txt -no-bidi :$LANGUAGE
             fi;
         fi
+    done;
+fi
+
+
+
+if [[ $FRAMEIT ]]; then
+    LANGUAGE_TMP=""
+    INCR=0
+    find . -name "*.png-framed.png" -exec rm '{}' \;
+    for file in metadata/android/*/images/phoneScreenshots/*.png; do
+        LANGUAGE="${file//metadata\/android\//}"
+        LANGUAGE="${LANGUAGE:0:2}"
+
+        echo "Processing $file"
+
+        if [ "$LANGUAGE_TMP" != "$LANGUAGE" ]; then
+            INCR=0
+        fi
+
+        TITLE_GLOBAL=$(eval "cat Framefile.json | jq -r '.global[$INCR].title'")
+        FONT_GLOBAL=$(eval "cat Framefile.json | jq -r '.global[$INCR].font'")
+
+        TITLE_LOCAL=$(eval "cat Framefile.json | jq -r '.$LANGUAGE[$INCR].title'")
+        FONT_LOCAL=$(eval "cat Framefile.json | jq -r '.$LANGUAGE[$INCR].font'")
+
+        if [ ! -z "$TITLE_LOCAL" -a "$TITLE_LOCAL" != "null" ]; then
+            TITLE=$TITLE_LOCAL
+        else
+            TITLE=$TITLE_GLOBAL
+        fi
+
+        if [ ! -z "$FONT_LOCAL" -a "$FONT_LOCAL" != "null" ]; then
+            FONT=$FONT_LOCAL
+        else
+            FONT=$FONT_GLOBAL
+        fi
+
+        if [ "$LANGUAGE" == "ar" ] || [ "$LANGUAGE" == "fa" ] || [ "$LANGUAGE" == "he" ]; then
+            TITLE=$(echo $TITLE | rev)
+        fi
+
+        LANGUAGE_TMP=$LANGUAGE
+        INCR=$((INCR+1))
+        OUTPUT="$file-framed.png"
+
+        convert $file \( +clone -alpha extract -draw 'fill black polygon 0,0 0,85 85,0 fill white circle 85,85 85,0' \( +clone -flip \) -compose Multiply -composite \( +clone -flop \) -compose Multiply -composite \) -alpha off -compose CopyOpacity -composite -resize 1064x2125 rounded_temp.png \
+        && \
+        convert google_pixel_2_xl_frame.png rounded_temp.png -gravity center -geometry +13+700 -composite -font $FONT -pointsize 135 -fill '#303E55' -annotate +0-1000 ''"$TITLE"'' $OUTPUT
     done;
 fi
