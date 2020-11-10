c03c547892968b7f88ca8936c8e2b41018ea6c38
Tinashe Mzondiwa <tmzon08@gmail.com>
2020-11-10 02:09:37 -0500

    Fix junit dependency version

--- buildSrc/src/main/kotlin/dependencies/TestAndroidDependencies.kt
+++ buildSrc/src/main/kotlin/dependencies/TestAndroidDependencies.kt
@@ -5,6 +5,6 @@ object TestAndroidDependencies {
     const val ESPRESSO = "androidx.test.espresso:espresso-core:${Versions.ESPRESSO}"
     const val RUNNER = "androidx.test:runner:${Versions.ANDROIDX_TEST}"
     const val RULES = "androidx.test:rules:${Versions.ANDROIDX_TEST}"
-    const val JUNIT = "androidx.test.ext:junit:${Versions.ANDROIDX_TEST}"
+    const val JUNIT = "androidx.test.ext:junit:${Versions.EXT}"
     const val FRAGMENT_TEST = "androidx.fragment:fragment-testing:${Versions.FRAGMENT_TEST}"
 }
