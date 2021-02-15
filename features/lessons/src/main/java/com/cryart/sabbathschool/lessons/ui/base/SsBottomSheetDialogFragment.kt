bc6a9d4bdc113ad0f51cc5d16ca478c30b668d18
Vitalii Cherniak <chernyak.vitaliy@gmail.com>
2021-02-15 19:31:12 +0200

    Fixed #160: Fix bottom sheet navigation bar color

--- features/lessons/src/main/java/com/cryart/sabbathschool/lessons/ui/base/SsBottomSheetDialogFragment.kt
+++ features/lessons/src/main/java/com/cryart/sabbathschool/lessons/ui/base/SsBottomSheetDialogFragment.kt
@@ -22,19 +22,25 @@
 
 package com.cryart.sabbathschool.lessons.ui.base
 
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.DialogFragment
+import com.cryart.sabbathschool.lessons.R
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 abstract class SsBottomSheetDialogFragment : BottomSheetDialogFragment() {
+    init {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            setStyle(DialogFragment.STYLE_NORMAL, R.style.ThemeOverlay_SS_BottomSheetDialog)
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         (requireDialog() as BottomSheetDialog).apply {
-            // do we need this?
-            // behavior.disableShapeAnimations()
             dismissWithAnimation = true
         }
     }
