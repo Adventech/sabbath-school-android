8afefe1ed0ec734812259b68417fcde6cefc18e7
Tinashe Mzondiwa <tmzon08@gmail.com>
2021-07-10 17:22:21 -0400

    Update min widget width

--- features/app-widgets/src/main/java/app/ss/widgets/today/TodayAppWidget.kt
+++ features/app-widgets/src/main/java/app/ss/widgets/today/TodayAppWidget.kt
@@ -30,8 +30,8 @@ class TodayAppWidget : BaseWidgetProvider<TodayWidgetModel>() {
         model: TodayWidgetModel?
     ) {
         val views = RemoteViews(context.packageName, R.layout.today_app_widget)
-        views.setTextViewText(R.id.widget_lesson_date, model?.date ?: "----")
-        views.setTextViewText(R.id.widget_lesson_title, model?.title ?: "----")
+        views.setTextViewText(R.id.widget_lesson_date, model?.date ?: context.getString(R.string.ss_widget_error_label))
+        views.setTextViewText(R.id.widget_lesson_title, model?.title ?: context.getString(R.string.ss_widget_error_label))
 
         val pendingIntent: PendingIntent? = model?.let {
             Intent().apply {
