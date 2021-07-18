5dd14df8104feccd5be79607510b82bc51d08c1e
Tinashe Mzondiwa <tmzon08@gmail.com>
2021-07-18 19:13:34 -0400

    Bump app version

--- buildSrc/src/main/kotlin/dependencies/Versions.kt
+++ buildSrc/src/main/kotlin/dependencies/Versions.kt
@@ -1,19 +1,19 @@
 package dependencies
 
 object Versions {
-    const val COROUTINES = "1.5.0"
+    const val COROUTINES = "1.5.1"
 
-    const val MATERIAL = "1.3.0"
+    const val MATERIAL = "1.4.0"
     const val DESUGAR = "1.1.1"
 
     object AndroidX {
         const val APPCOMPAT = "1.2.0"
-        const val RECYCLER_VIEW = "1.1.0"
+        const val RECYCLER_VIEW = "1.2.1"
         const val LIFECYCLE = "2.3.1"
         const val LIFECYCLE_KTX = "2.4.0-alpha01"
         const val LIFECYCLE_EXT = "2.2.0"
-        const val CORE = "1.5.0"
-        const val FRAGMENT = "1.3.0"
+        const val CORE = "1.6.0"
+        const val FRAGMENT = "1.3.5"
         const val ACTIVITY = "1.2.0"
         const val CONSTRAINT_LAYOUT = "2.0.4"
         const val START_UP = "1.0.0"
@@ -23,29 +23,29 @@ object Versions {
     }
 
     object Facebook {
-        const val SDK = "11.0.0"
+        const val SDK = "11.1.0"
         const val SHIMMER = "0.5.0"
     }
 
     const val HILT = "2.37"
 
     const val TIMBER = "4.7.1"
-    const val COIL = "1.2.2"
-    const val TAP_TARGET = "3.2.0"
+    const val COIL = "1.3.0"
+    const val TAP_TARGET = "3.3.0"
     const val ICONICS = "5.2.8@aar"
 
-    const val FIREBASE_BOM = "28.1.0"
+    const val FIREBASE_BOM = "28.2.1"
     const val PLAY_AUTH = "19.0.0"
     const val ANDROID_JOB = "1.4.2"
-    const val JODA = "2.10.9"
+    const val JODA = "2.10.9.1"
 
     // Tests
-    const val ANDROIDX_TEST = "1.3.0"
+    const val ANDROIDX_TEST = "1.4.0"
     const val EXT = "1.1.2"
     const val ARCH_CORE = "2.1.0"
     const val JUNIT = "4.13.2"
-    const val ROBOELECTRIC = "4.5.1"
-    const val MOCKK = "1.11.0"
+    const val ROBOELECTRIC = "4.6.1"
+    const val MOCKK = "1.12.0"
     const val ESPRESSO = "3.3.0"
     const val FRAGMENT_TEST = "1.3.0"
     const val KLUENT = "1.67"
