8578c6b8786682409197e30667119d8b8c69f1a3
Tinashe Mzondiwa <tmzon08@gmail.com>
2021-08-02 01:32:29 -0400

    Bump dependency versions

--- buildSrc/src/main/kotlin/dependencies/Versions.kt
+++ buildSrc/src/main/kotlin/dependencies/Versions.kt
@@ -7,15 +7,15 @@ object Versions {
     const val DESUGAR = "1.1.1"
 
     object AndroidX {
-        const val APPCOMPAT = "1.2.0"
+        const val APPCOMPAT = "1.3.1"
         const val RECYCLER_VIEW = "1.2.1"
         const val LIFECYCLE = "2.3.1"
         const val LIFECYCLE_KTX = "2.4.0-alpha01"
         const val LIFECYCLE_EXT = "2.2.0"
         const val CORE = "1.6.0"
-        const val FRAGMENT = "1.3.5"
+        const val FRAGMENT = "1.3.6"
         const val ACTIVITY = "1.2.0"
-        const val CONSTRAINT_LAYOUT = "2.0.4"
+        const val CONSTRAINT_LAYOUT = "2.1.0"
         const val START_UP = "1.0.0"
         const val PREFERENCE = "1.1.1"
         const val BROWSER = "1.3.0"
@@ -23,18 +23,18 @@ object Versions {
     }
 
     object Facebook {
-        const val SDK = "11.1.0"
+        const val SDK = "11.1.1"
         const val SHIMMER = "0.5.0"
     }
 
-    const val HILT = "2.37"
+    const val HILT = "2.38.1"
 
     const val TIMBER = "4.7.1"
-    const val COIL = "1.3.0"
+    const val COIL = "1.3.1"
     const val TAP_TARGET = "3.3.0"
-    const val ICONICS = "5.2.8@aar"
+    const val ICONICS = "5.3.0@aar"
 
-    const val FIREBASE_BOM = "28.2.1"
+    const val FIREBASE_BOM = "28.3.0"
     const val PLAY_AUTH = "19.0.0"
     const val ANDROID_JOB = "1.4.2"
     const val JODA = "2.10.9.1"
@@ -48,8 +48,8 @@ object Versions {
     const val JUNIT = "4.13.2"
     const val ROBOELECTRIC = "4.6.1"
     const val MOCKK = "1.12.0"
-    const val ESPRESSO = "3.3.0"
+    const val ESPRESSO = "3.4.0"
     const val FRAGMENT_TEST = "1.3.0"
-    const val KLUENT = "1.67"
-    const val TURBINE = "0.5.2"
+    const val KLUENT = "1.68"
+    const val TURBINE = "0.6.0"
 }
