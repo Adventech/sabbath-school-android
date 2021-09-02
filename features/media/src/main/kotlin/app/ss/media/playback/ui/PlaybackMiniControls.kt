e9de93a58f6a55b399b69148637b0718e47716d4
Tinashe Mzondiwa <tmzon08@gmail.com>
2021-09-02 12:09:12 -0400

    ktlint

--- features/media/src/main/kotlin/app/ss/media/playback/ui/PlaybackMiniControls.kt
+++ features/media/src/main/kotlin/app/ss/media/playback/ui/PlaybackMiniControls.kt
@@ -18,7 +18,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
