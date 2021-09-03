e9f98cf716c3da79daef6a712d0e42defcf406ad
Tinashe Mzondiwa <tmzon08@gmail.com>
2021-09-03 01:15:31 -0400

    Add custom bottom sheet fragment

---
+++ common/design/src/main/kotlin/com/cryart/design/base/TransparentBottomSheetFragment.kt
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2021. Adventech <info@adventech.io>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.cryart.design.base
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat.Type.systemBars
+import androidx.core.view.updateLayoutParams
+import androidx.core.view.updateMargins
+import com.cryart.design.R
+import com.cryart.design.ext.doOnApplyWindowInsets
+import com.cryart.design.theme.SSTheme
+import com.cryart.design.theme.Spacing16
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+open class BaseTransparentBottomSheetFragment : BottomSheetDialogFragment() {
+
+    override fun getTheme() = R.style.TransparentBottomSheetDialogTheme
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.doOnApplyWindowInsets { insetView, windowInsets, _, initialMargins ->
+            insetView.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                updateMargins(top = initialMargins.top + windowInsets.getInsets(systemBars()).top)
+            }
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        object : BottomSheetDialog(requireContext(), theme) {
+            override fun onAttachedToWindow() {
+                super.onAttachedToWindow()
+                window?.let {
+                    WindowCompat.setDecorFitsSystemWindows(it, false)
+                }
+                findViewById<View>(com.google.android.material.R.id.container)?.fitsSystemWindows = false
+                findViewById<View>(com.google.android.material.R.id.coordinator)?.fitsSystemWindows = false
+            }
+        }.apply {
+            setOnShowListener {
+                (this@BaseTransparentBottomSheetFragment.dialog as BottomSheetDialog).behavior.setState(
+                    BottomSheetBehavior.STATE_EXPANDED
+                )
+            }
+        }
+}
+
+@Composable
+fun TransparentBottomSheetSurface(content: @Composable () -> Unit) {
+    SSTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            shape = RoundedCornerShape(
+                topStart = Spacing16,
+                topEnd = Spacing16
+            ),
+            content = content
+        )
+    }
+}
