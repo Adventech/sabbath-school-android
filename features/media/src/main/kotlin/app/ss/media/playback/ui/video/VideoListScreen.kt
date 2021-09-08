31d5fce227890f6ade22e9e322cc2ad0d8495c12
Tinashe Mzondiwa <tmzon08@gmail.com>
2021-09-08 17:36:17 -0400

    Ui for vertical video list

--- features/media/src/main/kotlin/app/ss/media/playback/ui/video/VideoListScreen.kt
+++ features/media/src/main/kotlin/app/ss/media/playback/ui/video/VideoListScreen.kt
@@ -24,10 +24,12 @@ package app.ss.media.playback.ui.video
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -38,7 +40,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -47,7 +48,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -56,17 +61,22 @@ import app.ss.media.model.SSVideo
 import app.ss.media.model.SSVideosInfo
 import app.ss.media.playback.ui.common.RemoteImage
 import app.ss.media.playback.ui.common.rememberFlowWithLifecycle
-import coil.annotation.ExperimentalCoilApi
+import com.cryart.design.ext.thenIf
 import com.cryart.design.theme.BaseBlue
 import com.cryart.design.theme.BaseGrey2
 import com.cryart.design.theme.Body
+import com.cryart.design.theme.Dimens
 import com.cryart.design.theme.Spacing16
 import com.cryart.design.theme.Spacing24
+import com.cryart.design.theme.Spacing32
+import com.cryart.design.theme.Spacing4
 import com.cryart.design.theme.Spacing8
 import com.cryart.design.theme.Title
 import com.cryart.design.theme.TitleSmall
+import com.cryart.design.theme.isLargeScreen
 import com.cryart.design.theme.navTitle
 import com.cryart.design.widgets.DragHandle
+import com.cryart.sabbathschool.core.extensions.list.subList
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.distinctUntilChanged
@@ -109,7 +119,7 @@ internal fun ViewListScreen(
             Text(
                 text = stringResource(id = R.string.ss_media_video),
                 style = Title.copy(
-                    color = MaterialTheme.colors.onSurface,
+                    color = navTitle(),
                     fontSize = 30.sp
                 ),
                 modifier = Modifier.padding(horizontal = Spacing24)
@@ -120,8 +130,28 @@ internal fun ViewListScreen(
             Spacer(modifier = Modifier.height(Spacing16))
         }
 
-        items(videoList) { videosInfo ->
-            VideosInfoList(videosInfo = videosInfo)
+        if (videoList.size == 1 && videoList.first().clips.isNotEmpty()) {
+            item {
+                VideoColumn(
+                    video = videoList.first().clips.first(),
+                    featured = true,
+                    vertical = true
+                )
+            }
+
+            item {
+                Spacer(modifier = Modifier.height(Spacing32))
+            }
+
+            items(videoList.first().clips.subList(1)) { video ->
+                VideoRow(video = video)
+
+                Spacer(modifier = Modifier.height(Dimens.grid_4))
+            }
+        } else {
+            items(videoList) { videosInfo ->
+                VideosInfoList(videosInfo = videosInfo)
+            }
         }
     }
 
@@ -163,16 +193,33 @@ private fun VideosInfoList(
     }
 }
 
-@OptIn(ExperimentalCoilApi::class)
 @Composable
 private fun VideoColumn(
     video: SSVideo,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    featured: Boolean = false,
+    vertical: Boolean = false
 ) {
+    val defSize = getThumbnailSize(vertical = vertical)
+    val size = if (featured) {
+        val screenWidth = screenWidth() - (24 * 2)
+        Size(
+            width = screenWidth.toFloat(),
+            height = (screenWidth / (defSize.width / defSize.height))
+        )
+    } else {
+        defSize
+    }
+
     Column(
         modifier = modifier
             .width(IntrinsicSize.Min)
             .padding(horizontal = Spacing8)
+            .thenIf(featured) {
+                Modifier.padding(
+                    horizontal = Spacing24
+                )
+            }
             .clickable { }
     ) {
 
@@ -181,8 +228,8 @@ private fun VideoColumn(
             contentDescription = video.title,
             modifier = modifier
                 .size(
-                    width = 276.dp,
-                    height = 149.dp
+                    width = size.width.dp,
+                    height = size.height.dp
                 ),
             cornerRadius = CoverCornerRadius
         )
@@ -193,7 +240,7 @@ private fun VideoColumn(
             text = video.title,
             style = TitleSmall.copy(
                 color = navTitle(),
-                fontSize = 16.sp
+                fontSize = if (featured) 19.sp else 16.sp
             )
         )
         Text(
@@ -205,4 +252,79 @@ private fun VideoColumn(
     }
 }
 
+@Composable
+private fun VideoRow(
+    video: SSVideo,
+    modifier: Modifier = Modifier,
+) {
+    val size = getThumbnailSize(vertical = true)
+
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(Dimens.grid_2),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .padding(
+                horizontal = Spacing24,
+            )
+            .clickable { }
+    ) {
+        RemoteImage(
+            data = video.thumbnail,
+            contentDescription = video.title,
+            modifier = modifier
+                .size(
+                    width = size.width.dp,
+                    height = size.height.dp
+                ),
+        )
+
+        Column(
+            modifier = Modifier.weight(1f),
+            verticalArrangement = Arrangement.spacedBy(Spacing4)
+        ) {
+            Text(
+                text = video.title,
+                style = TitleSmall.copy(
+                    color = navTitle(),
+                    fontSize = 16.sp,
+                    lineHeight = TextUnit.Unspecified
+                ),
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = video.artist,
+                style = Body.copy(
+                    fontSize = 14.sp
+                ),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
+}
+
 private const val CoverCornerRadius = 6f
+
+@Composable
+private fun getThumbnailSize(vertical: Boolean): Size {
+    val ratio = if (vertical) 2.7f else 1.2f
+    val largeScreen = isLargeScreen()
+
+    val width = if (largeScreen) defSize.width else screenWidth() / ratio
+    val height = if (largeScreen) defSize.height else (width / (defSize.width / defSize.height))
+
+    return Size(
+        width = width,
+        height = height
+    )
+}
+
+private val defSize = Size(276f, 149f)
+
+/**
+ * On large screens this returns approx width of the BottomSheet and not the actual device width
+ */
+@Composable
+private fun screenWidth(): Int =
+    if (isLargeScreen()) 600 else LocalConfiguration.current.screenWidthDp
