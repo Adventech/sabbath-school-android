32c8c667c7b5700cc1940197ccb1bf46ca28978e
Tinashe Mzondiwa <tmzon08@gmail.com>
2021-09-19 02:52:15 -0400

    Update dependency versions

--- buildSrc/src/main/kotlin/dependencies/Versions.kt
+++ buildSrc/src/main/kotlin/dependencies/Versions.kt
@@ -4,7 +4,7 @@ object Versions {
     const val COROUTINES = "1.5.2"
     const val COMPOSE = "1.0.2"
     const val MATERIAL = "1.4.0"
-    const val ACCOMPANIST = "0.17.0"
+    const val ACCOMPANIST = "0.18.0"
 
     object AndroidX {
         const val APPCOMPAT = "1.3.1"
@@ -21,7 +21,7 @@ object Versions {
         const val PREFERENCE = "1.1.1"
         const val BROWSER = "1.3.0"
         const val DATASTORE_PREFS = "1.0.0"
-        const val MEDIA = "1.4.1"
+        const val MEDIA = "1.4.2"
         const val ROOM = "2.3.0"
     }
 
@@ -35,9 +35,9 @@ object Versions {
     const val TIMBER = "5.0.1"
     const val COIL = "1.3.2"
     const val TAP_TARGET = "3.3.0"
-    const val ICONICS = "5.3.0@aar"
+    const val ICONICS = "5.3.1@aar"
 
-    const val FIREBASE_BOM = "28.4.0"
+    const val FIREBASE_BOM = "28.4.1"
     const val PLAY_AUTH = "19.2.0"
     const val ANDROID_JOB = "1.4.2"
     const val JODA = "2.10.9.1"
