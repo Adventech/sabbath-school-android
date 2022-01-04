e7a7687083f39ccb385e51de8c93bd78dc648dc7
Tinashe Mzondiwa <tmzon08@gmail.com>
2022-01-03 23:20:08 -0500

    Separate provider class

---
+++ features/app-widgets/src/main/java/app/ss/widgets/glance/TodayWidgetProvider.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2022. Adventech <info@adventech.io>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package app.ss.widgets.glance
+
+import android.net.Uri
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import app.ss.widgets.WidgetDataProvider
+import app.ss.widgets.model.TodayWidgetModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+internal class TodayWidgetProvider : GlanceAppWidgetReceiver() {
+
+    @Inject
+    lateinit var dataProvider: WidgetDataProvider
+
+    private val model = TodayWidgetModel(
+        "Further Thought",
+        "Friday, December 31",
+        "",
+        Uri.EMPTY
+    )
+
+    override val glanceAppWidget: GlanceAppWidget get() = TodayWidget(model)
+}
