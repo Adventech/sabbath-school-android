2c079c58a59b2ae98280a6e82fe92d4f1bbfbaf0
Tinashe Mzondiwa <tmzon08@gmail.com>
2022-02-13 12:43:26 +0200

    Add repository

---
+++ libraries/test_utils/src/main/java/com/cryart/sabbathschool/test/di/repository/FakeAuthRepository.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2022. Adventech <info@adventech.io>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.cryart.sabbathschool.test.di.repository
+
+import app.ss.auth.AuthRepository
+import app.ss.auth.AuthResponse
+import app.ss.models.auth.SSUser
+import com.cryart.sabbathschool.core.response.Resource
+import javax.inject.Inject
+
+class FakeAuthRepository @Inject constructor() : AuthRepository {
+
+    override suspend fun getUser(): Resource<SSUser?> {
+        return Resource.success(null)
+    }
+
+    override suspend fun signIn(): Resource<AuthResponse> {
+        return Resource.success(AuthResponse.Error)
+    }
+
+    override suspend fun signIn(token: String): Resource<AuthResponse> {
+        return Resource.success(AuthResponse.Error)
+    }
+
+    override suspend fun logout() {}
+}
