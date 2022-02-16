70e285a477ff8501836e5f94df2d89311198fca9
Tinashe Mzondiwa <tmzon08@gmail.com>
2022-02-16 12:13:52 +0200

    Add db entities for comments and highlights

--- common/storage/build.gradle.kts
+++ common/storage/build.gradle.kts
@@ -22,6 +22,9 @@
 
 import dependencies.Dependencies.Hilt
 import dependencies.Dependencies.AndroidX.Room
+import dependencies.Dependencies.Square.Moshi
+import extensions.implementation
+import extensions.kapt
 
 plugins {
     id(BuildPlugins.Android.LIBRARY)
@@ -62,4 +65,7 @@ dependencies {
     implementation(Room.runtime)
     implementation(Room.ktx)
     kapt(Room.compiler)
+
+    implementation(Moshi.kotlin)
+    kapt(Moshi.codegen)
 }
