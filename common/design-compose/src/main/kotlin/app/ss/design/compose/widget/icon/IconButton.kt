a348581823cd56fa1fb8decfe7fac9871b71f37f
Tinashe Mzondiwa <tmzon08@gmail.com>
2022-06-22 23:54:57 -0400

    Add custom IconButton

--- common/design-compose/src/main/kotlin/app/ss/design/compose/widget/icon/IconButton.kt
+++ common/design-compose/src/main/kotlin/app/ss/design/compose/widget/icon/IconButton.kt
@@ -22,14 +22,29 @@
 
 package app.ss.design.compose.widget.icon
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import app.ss.design.compose.extensions.internal.minimumTouchTargetSize
 
 /**
  * An IconButton [IconSlot]
@@ -58,3 +73,41 @@ data class IconButton(
         }
     }
 }
+
+/**
+ * An [androidx.compose.material3.IconButton] with a customizable @param stateLayerSize.
+ */
+@Composable
+fun IconButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    colors: IconButtonColors = IconButtonDefaults.iconButtonColors(),
+    stateLayerSize: Dp = StateLayerSize,
+    content: @Composable () -> Unit
+) {
+    Box(
+        modifier =
+        modifier
+            .minimumTouchTargetSize()
+            .size(stateLayerSize)
+            .background(color = colors.containerColor(enabled).value)
+            .clickable(
+                onClick = onClick,
+                enabled = enabled,
+                role = Role.Button,
+                interactionSource = interactionSource,
+                indication = rememberRipple(
+                    bounded = false,
+                    radius = stateLayerSize / 2
+                )
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        val contentColor = colors.contentColor(enabled).value
+        CompositionLocalProvider(LocalContentColor provides contentColor, content = content)
+    }
+}
+
+private val StateLayerSize = 40.0.dp
