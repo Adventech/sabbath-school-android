b04207d122ada4576c5e13137c1f850800b1373d
Tinashe Mzondiwa <tmzon08@gmail.com>
2022-07-11 00:17:20 -0400

    Add translations

--- common/design-compose/src/main/kotlin/app/ss/design/compose/widget/icon/Icons.kt
+++ common/design-compose/src/main/kotlin/app/ss/design/compose/widget/icon/Icons.kt
@@ -22,6 +22,7 @@
 
 package app.ss.design.compose.widget.icon
 
+import androidx.annotation.StringRes
 import androidx.compose.material.icons.rounded.AccountCircle
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.ArrowDropDown
@@ -34,26 +35,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.compose.material.icons.Icons as MaterialIcons
+import app.ss.translations.R.string as Translations
 
 enum class Icons(
     private val imageVector: ImageVector,
-    private val contentDescription: String?
+    @StringRes private val contentDescription: Int?,
 ) : IconSlot {
 
-    AccountCircle(MaterialIcons.Rounded.AccountCircle, "Account"),
-    ArrowBack(MaterialIcons.Rounded.ArrowBack, "Arrow Back"),
-    ArrowDropDown(MaterialIcons.Rounded.ArrowDropDown, "Arrow Drop Down"),
-    ArrowRight(MaterialIcons.Rounded.KeyboardArrowRight, "Arrow Right"),
-    Cancel(MaterialIcons.Rounded.Cancel, "Cancel"),
-    Check(MaterialIcons.Rounded.Check, "Selected"),
-    Close(MaterialIcons.Rounded.Close, "Close");
+    AccountCircle(MaterialIcons.Rounded.AccountCircle, Translations.ss_account),
+    ArrowBack(MaterialIcons.Rounded.ArrowBack, Translations.ss_action_arrow_back),
+    ArrowDropDown(MaterialIcons.Rounded.ArrowDropDown, Translations.ss_action_arrow_drop_down),
+    ArrowRight(MaterialIcons.Rounded.KeyboardArrowRight, Translations.ss_action_arrow_right),
+    Cancel(MaterialIcons.Rounded.Cancel, android.R.string.cancel),
+    Check(MaterialIcons.Rounded.Check, Translations.ss_action_selected),
+    Close(MaterialIcons.Rounded.Close, Translations.ss_action_close);
 
     @Composable
     override fun Content(contentColor: Color, modifier: Modifier) {
         Icon(
             imageVector = imageVector,
-            contentDescription = contentDescription,
+            contentDescription = contentDescription?.let { stringResource(id = it) },
             tint = contentColor,
             modifier = modifier
         )
