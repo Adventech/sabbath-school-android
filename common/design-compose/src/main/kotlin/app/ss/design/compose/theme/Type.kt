0edb67bcbb820625f0a97003732f73a6702f7c92
Tinashe Mzondiwa <tmzon08@gmail.com>
2022-08-20 22:47:04 -0400

    Remove extra letter spacings

--- common/design-compose/src/main/kotlin/app/ss/design/compose/theme/Type.kt
+++ common/design-compose/src/main/kotlin/app/ss/design/compose/theme/Type.kt
@@ -42,105 +42,90 @@ val SsTypography = Typography(
         fontFamily = LatoFontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 57.sp,
-        lineHeight = 64.sp,
-        letterSpacing = (-0.25).sp
+        lineHeight = 64.sp
     ),
     displayMedium = TextStyle(
         fontFamily = LatoFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 45.sp,
-        lineHeight = 52.sp,
-        letterSpacing = 0.sp
+        lineHeight = 52.sp
     ),
     displaySmall = TextStyle(
         fontFamily = LatoFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 36.sp,
-        lineHeight = 44.sp,
-        letterSpacing = 0.sp
+        lineHeight = 44.sp
     ),
     headlineLarge = TextStyle(
         fontFamily = LatoFontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 32.sp,
-        lineHeight = 40.sp,
-        letterSpacing = 0.sp
+        lineHeight = 40.sp
     ),
     headlineMedium = TextStyle(
         fontFamily = LatoFontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 28.sp,
-        lineHeight = 36.sp,
-        letterSpacing = 0.sp
+        lineHeight = 36.sp
     ),
     headlineSmall = TextStyle(
         fontFamily = LatoFontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 24.sp,
-        lineHeight = 32.sp,
-        letterSpacing = 0.sp
+        lineHeight = 32.sp
     ),
     titleLarge = TextStyle(
         fontFamily = LatoFontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 20.sp,
-        lineHeight = 26.sp,
-        letterSpacing = 0.sp
+        lineHeight = 26.sp
     ),
     titleMedium = TextStyle(
         fontFamily = LatoFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.1.sp
+        lineHeight = 24.sp
     ),
     titleSmall = TextStyle(
         fontFamily = LatoFontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 14.sp,
-        lineHeight = 20.sp,
-        letterSpacing = 0.1.sp
+        lineHeight = 20.sp
     ),
     labelLarge = TextStyle(
         fontFamily = LatoFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
-        lineHeight = 20.sp,
-        letterSpacing = 0.1.sp
+        lineHeight = 20.sp
     ),
     bodyLarge = TextStyle(
         fontFamily = LatoFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        lineHeight = 24.sp
     ),
     bodyMedium = TextStyle(
         fontFamily = LatoFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
-        lineHeight = 20.sp,
-        letterSpacing = 0.25.sp
+        lineHeight = 20.sp
     ),
     bodySmall = TextStyle(
         fontFamily = LatoFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 13.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.4.sp
+        lineHeight = 16.sp
     ),
     labelMedium = TextStyle(
         fontFamily = LatoFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 17.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        lineHeight = 16.sp
     ),
     labelSmall = TextStyle(
         fontFamily = LatoFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        lineHeight = 16.sp
     )
 )
