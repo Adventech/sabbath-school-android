1774a243a0c6b259e30fe80985855239ad379a6f
Tinashe Mzondiwa <tmzon08@gmail.com>
2022-08-28 00:39:38 -0400

    Bump version

--- buildSrc/src/main/kotlin/BuildAndroidConfig.kt
+++ buildSrc/src/main/kotlin/BuildAndroidConfig.kt
@@ -11,8 +11,8 @@ object BuildAndroidConfig {
 
     object Version {
         private const val MAJOR = 4
-        private const val MINOR = 13
-        private const val PATCH = 1
+        private const val MINOR = 14
+        private const val PATCH = 0
 
         const val name = "$MAJOR.$MINOR.$PATCH"
     }
