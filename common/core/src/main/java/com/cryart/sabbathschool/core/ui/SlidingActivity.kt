3bb5c29879742c8225266456a287666016a19d71
Tinashe Mzondiwa <tmzon08@gmail.com>
2022-10-04 20:38:04 -0400

    Remove deprecated onBackPressed

--- common/core/src/main/java/com/cryart/sabbathschool/core/ui/SlidingActivity.kt
+++ common/core/src/main/java/com/cryart/sabbathschool/core/ui/SlidingActivity.kt
@@ -1,6 +1,7 @@
 package com.cryart.sabbathschool.core.ui
 
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import com.cryart.sabbathschool.core.R
 
 abstract class SlidingActivity : SSBaseActivity() {
@@ -8,11 +9,12 @@ abstract class SlidingActivity : SSBaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         overridePendingTransition(R.anim.enter_from_right, android.R.anim.fade_out)
-    }
 
-    override fun onBackPressed() {
-        super.onBackPressed()
-        overridePendingTransition(android.R.anim.fade_in, R.anim.exit_to_right)
+        onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                finish()
+            }
+        })
     }
 
     override fun finish() {
