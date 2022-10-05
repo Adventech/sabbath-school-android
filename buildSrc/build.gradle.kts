0c556a3cd30fa166cdaea82c12a2cee791caee35
Tinashe Mzondiwa <tmzon08@gmail.com>
2022-10-04 20:16:08 -0400

    Update kotlin and compose compiler

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 object PluginVersions {
     const val GRADLE_ANDROID = "7.3.0"
-    const val KOTLIN = "1.7.10"
+    const val KOTLIN = "1.7.20"
     const val KTLINT = "11.0.0"
     const val HILT = "2.44"
     const val PAPARAZZI = "1.0.0"
