160dcc0496793af97e3e9718655bf8a4a28c4d8c
Tinashe Mzondiwa <tmzon08@gmail.com>
2023-05-21 17:32:44 -0400

    Create tv-app module

---
+++ app-tv/build.gradle.kts
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2023. Adventech <info@adventech.io>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+plugins {
+    alias(libs.plugins.sgp.base)
+    id("com.android.application")
+    id("dagger.hilt.android.plugin")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+android {
+    namespace = "app.ss.tv"
+
+    defaultConfig {
+        applicationId = "app.ss.tv"
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+slack {
+    features { compose() }
+}
+
+dependencies {
+    implementation(projects.common.designCompose)
+    implementation(projects.common.lessonsData)
+    implementation(projects.common.translations)
+
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.tv.foundation)
+    implementation(libs.androidx.compose.tv.material)
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.core.splashscreen)
+
+    implementation(libs.google.hilt.android)
+    kapt(libs.google.hilt.compiler)
+}
