1e37a7462250dd833cd8030c2939f5a928cd1b60
Tinashe Mzondiwa <tmzon08@gmail.com>
2023-11-11 21:22:14 -0500

    Update reading screen loading view (#876)

--- features/lessons/src/main/java/com/cryart/sabbathschool/lessons/ui/readings/components/ProgressBarComponent.kt
+++ features/lessons/src/main/java/com/cryart/sabbathschool/lessons/ui/readings/components/ProgressBarComponent.kt
@@ -22,28 +22,93 @@
 
 package com.cryart.sabbathschool.lessons.ui.readings.components
 
-import android.graphics.Color
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.dp
 import androidx.core.view.isVisible
 import androidx.lifecycle.LifecycleOwner
-import com.cryart.design.theme
+import app.ss.design.compose.extensions.modifier.asPlaceholder
+import app.ss.design.compose.extensions.previews.DayNightPreviews
+import app.ss.design.compose.theme.SsTheme
+import app.ss.design.compose.widget.placeholder.PlaceholderDefaults
 import com.cryart.sabbathschool.lessons.databinding.SsProgressBarBinding
 import com.cryart.sabbathschool.lessons.ui.readings.SSReadingViewModel
 import com.cryart.sabbathschool.lessons.ui.readings.model.ReadingsState
 import ss.foundation.coroutines.flow.collectIn
-import ss.prefs.api.SSPrefs
+import com.cryart.sabbathschool.lessons.R as LessonsR
 
 class ProgressBarComponent(
     private val binding: SsProgressBarBinding,
     viewModel: SSReadingViewModel,
-    ssPrefs: SSPrefs,
     owner: LifecycleOwner
 ) {
     init {
         viewModel.viewState.collectIn(owner) { state ->
             binding.root.isVisible = state == ReadingsState.Loading
         }
-        binding.ssProgressIndicator.run {
-            theme(Color.parseColor(ssPrefs.getThemeColor().primary))
+        binding.composeView.setContent { SsTheme { Surface { Content() } } }
+    }
+}
+
+@Composable
+private fun Content(modifier: Modifier = Modifier) {
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize(),
+        contentPadding = WindowInsets.systemBars.asPaddingValues()
+    ) {
+        item {
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(dimensionResource(id = LessonsR.dimen.ss_reading_toolbar_height))
+                    .asPlaceholder(true, shape = RectangleShape)
+            )
+        }
+
+        item {
+            Spacer(
+                modifier = Modifier
+                    .height(16.dp)
+                    .fillMaxWidth()
+            )
+        }
+
+        items(15) {
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+            ) {
+                Spacer(
+                    modifier = Modifier
+                        .height(14.dp)
+                        .weight(1f)
+                        .asPlaceholder(true, shape = PlaceholderDefaults.shape)
+                )
+
+                Spacer(modifier = Modifier.width((0..200).random().dp))
+            }
         }
     }
+}
+
+@DayNightPreviews
+@Composable
+private fun Preview() {
+    SsTheme { Content() }
 }
