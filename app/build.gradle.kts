f9b6c6195c80af0f79953270f002e11017dc6710
Tinashe Mzondiwa <tmzon08@gmail.com>
2023-12-02 17:12:46 -0500

    Remove no-op tv declaration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -217,7 +217,7 @@ object BuildAndroidConfig {
 
     object Version {
         private const val MAJOR = 4
-        private const val MINOR = 41
+        private const val MINOR = 42
         private const val PATCH = 0
 
         const val name = "$MAJOR.$MINOR.$PATCH"
