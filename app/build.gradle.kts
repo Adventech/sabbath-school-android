43259533e8517cf7a0698f8c1521cb0b226f9c16
Tinashe Mzondiwa <tmzon08@gmail.com>
2024-06-20 23:32:30 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ val appVersionCode = readPropertyValue(
     filePath = BuildAndroidConfig.VERSION_PROPS_FILE,
     key = "BUILD_NUMBER",
     defaultValue = "1"
-).toInt() + 21853
+).toInt() + 21856
 
 val webClientId = readPropertyValue(
     filePath = "$rootDir/${BuildAndroidConfig.API_KEYS_PROPS_FILE}",
@@ -226,7 +226,7 @@ object BuildAndroidConfig {
 
     object Version {
         private const val MAJOR = 4
-        private const val MINOR = 52
+        private const val MINOR = 53
         private const val PATCH = 0
 
         const val name = "$MAJOR.$MINOR.$PATCH"
