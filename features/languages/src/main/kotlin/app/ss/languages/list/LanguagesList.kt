232a0ac4d860b054d3479d60a297792ca4c16f6c
Tinashe Mzondiwa <tmzon08@gmail.com>
2024-08-04 01:54:23 -0400

    Migrate from deprecated animatePlacement

--- features/languages/src/main/kotlin/app/ss/languages/list/LanguagesList.kt
+++ features/languages/src/main/kotlin/app/ss/languages/list/LanguagesList.kt
@@ -22,7 +22,6 @@
 
 package app.ss.languages.list
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -60,12 +59,11 @@ internal fun LanguagesList(
     ) {
         items(models, key = { it.code }) { model ->
 
-            @OptIn(ExperimentalFoundationApi::class)
             LanguageItem(
                 model = model,
                 modifier = Modifier
                     .clickable { onItemClick(model) }
-                    .animateItemPlacement()
+                    .animateItem()
             )
 
             Divider()
