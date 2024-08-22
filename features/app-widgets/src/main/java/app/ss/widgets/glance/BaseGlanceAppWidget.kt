c4da2046f5311058e6f7719cb067a3cc9c7de5e5
Tinashe Mzondiwa <tmzon08@gmail.com>
2024-08-21 21:02:16 -0400

    Use WeekWidgetState

--- features/app-widgets/src/main/java/app/ss/widgets/glance/BaseGlanceAppWidget.kt
+++ features/app-widgets/src/main/java/app/ss/widgets/glance/BaseGlanceAppWidget.kt
@@ -2,7 +2,6 @@ package app.ss.widgets.glance
 
 import android.content.Context
 import androidx.glance.appwidget.GlanceAppWidget
-import app.ss.widgets.WidgetDataProvider
 import app.ss.widgets.data.AppWidgetRepository
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
@@ -12,7 +11,6 @@ import dagger.hilt.components.SingletonComponent
 @EntryPoint
 @InstallIn(SingletonComponent::class)
 internal interface AppWidgetDataProviderEntryPoint {
-    fun widgetDataProvider(): WidgetDataProvider
     fun widgetRepository(): AppWidgetRepository
 }
 
@@ -20,13 +18,6 @@ internal interface AppWidgetDataProviderEntryPoint {
  * A base [GlanceAppWidget] class that handles injection.
  */
 internal abstract class BaseGlanceAppWidget : GlanceAppWidget() {
-
-    fun dataProvider(context: Context): WidgetDataProvider {
-        val appContext = context.applicationContext ?: throw IllegalStateException()
-        val hiltEntryPoint =
-            EntryPointAccessors.fromApplication(appContext, AppWidgetDataProviderEntryPoint::class.java)
-        return hiltEntryPoint.widgetDataProvider()
-    }
 
     fun repository(context: Context): AppWidgetRepository {
         val appContext = context.applicationContext ?: throw IllegalStateException()
