26ac38981afc77507ac93c327d8b34825ffd7b0c
Tinashe Mzondiwa <tmzon08@gmail.com>
2024-10-09 00:20:27 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -218,7 +218,7 @@ object BuildAndroidConfig {
 
     object Version {
         private const val MAJOR = 4
-        private const val MINOR = 59
+        private const val MINOR = 60
         private const val PATCH = 0
 
         const val name = "$MAJOR.$MINOR.$PATCH"
