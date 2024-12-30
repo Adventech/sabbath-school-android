9d342097ea2d577eda4daf449dd17113bf79b7f8
Tinashe Mzondiwa <tmzon08@gmail.com>
2024-12-30 02:10:06 +0200

    Remove unused func

--- libraries/block-kit/model/src/main/kotlin/io/adventech/blockkit/model/Style.kt
+++ libraries/block-kit/model/src/main/kotlin/io/adventech/blockkit/model/Style.kt
@@ -42,19 +42,6 @@ enum class TextStyleSize {
     @Json(name = "lg") LG,
     @Json(name = "xl") XL,
     UNKNOWN;
-
-    companion object {
-        fun fromString(value: String?): TextStyleSize {
-            return when (value) {
-                "xs" -> XS
-                "sm" -> SM
-                "base" -> BASE
-                "lg" -> LG
-                "xl" -> XL
-                else -> UNKNOWN
-            }
-        }
-    }
 }
 
 @JsonClass(generateAdapter = false)
