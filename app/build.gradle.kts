ec760eb92501e1e932de36d5b4984494e3c92701
Tinashe Mzondiwa <tmzon08@gmail.com>
2025-02-17 19:40:18 -0500

    Revert pdfkit version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -224,7 +224,7 @@ object BuildAndroidConfig {
     object Version {
         private const val MAJOR = 5
         private const val MINOR = 1
-        private const val PATCH = 0
+        private const val PATCH = 1
 
         const val name = "$MAJOR.$MINOR.$PATCH"
     }
