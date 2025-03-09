fb0e311bb7fa1c840157b24312bfec78ad784410
Tinashe Mzondiwa <tmzon08@gmail.com>
2025-03-09 12:40:38 -0400

    Bump min version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -219,8 +219,8 @@ object BuildAndroidConfig {
 
     object Version {
         private const val MAJOR = 5
-        private const val MINOR = 3
-        private const val PATCH = 1
+        private const val MINOR = 4
+        private const val PATCH = 0
 
         const val name = "$MAJOR.$MINOR.$PATCH"
     }
