940f428d67b47e51efbca6e42ea312f5b04b7efb
Tinashe Mzondiwa <tmzon08@gmail.com>
2025-04-30 22:37:21 -0400

    FullScreenDialog fixes

--- libraries/block-kit/ui/src/main/kotlin/io/adventech/blockkit/ui/dialog/FullScreenDialog.kt
+++ libraries/block-kit/ui/src/main/kotlin/io/adventech/blockkit/ui/dialog/FullScreenDialog.kt
@@ -22,15 +22,12 @@
 
 package io.adventech.blockkit.ui.dialog
 
-import android.app.Activity
-import android.content.Context
-import android.content.ContextWrapper
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
-import android.widget.FrameLayout
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.AlertDialogDefaults
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
@@ -38,6 +35,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.window.DialogProperties
 import androidx.compose.ui.window.DialogWindowProvider
@@ -59,25 +57,29 @@ fun FullScreenDialog(
             dismissOnBackPress = properties.dismissOnBackPress,
             dismissOnClickOutside = properties.dismissOnClickOutside,
             securePolicy = properties.securePolicy,
-            usePlatformDefaultWidth = true, // must be true as a part of work around
+            usePlatformDefaultWidth = false,
             decorFitsSystemWindows = false
         ),
         content = {
-            val activityWindow = getActivityWindow()
             val dialogWindow = getDialogWindow()
             val parentView = LocalView.current.parent as View
             SideEffect {
-                if (activityWindow != null && dialogWindow != null) {
-                    val attributes = WindowManager.LayoutParams()
-                    attributes.copyFrom(activityWindow.attributes)
-                    attributes.type = dialogWindow.attributes.type
-                    dialogWindow.attributes = attributes
-                    parentView.layoutParams = FrameLayout.LayoutParams(activityWindow.decorView.width, activityWindow.decorView.height)
+                if (dialogWindow != null) {
+                    @Suppress("DEPRECATION")
+                    dialogWindow.statusBarColor = Color.Transparent.toArgb()
+                    @Suppress("DEPRECATION")
+                    dialogWindow.navigationBarColor = Color.Transparent.toArgb()
+                    dialogWindow.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
                     WindowCompat.getInsetsController(dialogWindow, parentView).isAppearanceLightStatusBars = windowLightStatusBar
+                    WindowCompat.getInsetsController(dialogWindow, parentView).isAppearanceLightNavigationBars = windowLightStatusBar
                 }
             }
 
-            Surface(modifier = Modifier.fillMaxSize(), color = Color.Transparent) {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = Color.Transparent,
+                tonalElevation = AlertDialogDefaults.TonalElevation,
+            ) {
                 content()
             }
         }
@@ -87,13 +89,3 @@ fun FullScreenDialog(
 // Window utils
 @Composable
 private fun getDialogWindow(): Window? = (LocalView.current.parent as? DialogWindowProvider)?.window
-
-@Composable
-private fun getActivityWindow(): Window? = LocalView.current.context.getActivityWindow()
-
-private tailrec fun Context.getActivityWindow(): Window? =
-    when (this) {
-        is Activity -> window
-        is ContextWrapper -> baseContext.getActivityWindow()
-        else -> null
-    }
