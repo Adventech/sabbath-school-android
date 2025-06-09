d96ad76d951829638dec130322f9fceab2294b41
Tinashe Mzondiwa <tmzon08@gmail.com>
2025-06-09 01:24:47 -0400

    Update highlighted text in dark mode
    (cherry picked from commit 0cb49dfdbd29be8f2f05ac6212f99e3ccfcea972)

--- libraries/block-kit/ui/src/main/kotlin/io/adventech/blockkit/ui/MarkdownText.kt
+++ libraries/block-kit/ui/src/main/kotlin/io/adventech/blockkit/ui/MarkdownText.kt
@@ -195,6 +195,7 @@ internal fun rememberMarkdownText(
     attributedTextParser: AttributedTextParser = remember { AttributedTextParser() }
 ): AnnotatedString {
     val defaultFontFamily = Styler.defaultFontFamily()
+    val highlightedTextColor = Styler.highlightedTextColor()
     val fonts by rememberMarkdownFonts(markdownText, attributedTextParser, defaultFontFamily)
     val fontProvider: (String?) -> FontFamily = remember(fonts) { { it?.let { fonts[it] } ?: defaultFontFamily } }
     val fontSizeProvider: (TextStyleSize?) -> TextUnit = remember { { styleTemplate.defaultTextSizePoints(it).sp } }
@@ -215,7 +216,10 @@ internal fun rememberMarkdownText(
                 val textLength = this.length
                 if (highlight.startIndex in 0 until textLength && highlight.endIndex in (highlight.startIndex + 1)..textLength) {
                     addStyle(
-                        style = SpanStyle(background = highlight.color.toColor()),
+                        style = SpanStyle(
+                            background = highlight.color.toColor(),
+                            color = highlightedTextColor,
+                        ),
                         start = highlight.startIndex,
                         end = highlight.endIndex,
                     )
