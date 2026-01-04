395c6245bd80a442b287805511cfef3066a99816
Tinashe Mzondiwa <tmzon08@gmail.com>
2026-01-04 11:53:03 +0200

    Optimize StorySlideContent

--- libraries/block-kit/ui/src/main/kotlin/io/adventech/blockkit/ui/StorySlideContent.kt
+++ libraries/block-kit/ui/src/main/kotlin/io/adventech/blockkit/ui/StorySlideContent.kt
@@ -24,7 +24,6 @@ package io.adventech.blockkit.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -41,9 +40,9 @@ import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContent
 import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -54,6 +53,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
@@ -96,17 +96,7 @@ fun StorySlideContent(
         pageCount = { pages.size },
     )
 
-    val imageScrollState = rememberScrollState()
     var totalScrollableWidth by remember(pages) { mutableFloatStateOf((screenWidth * pages.size).toFloat()) }
-
-    LaunchedEffect(pagerState.currentPage, pagerState.currentPageOffsetFraction) {
-        // Calculate the target scroll position
-        val progress = (pagerState.currentPage + pagerState.currentPageOffsetFraction) / (pages.size - 1)
-        val maxScroll = totalScrollableWidth
-        val targetScroll = progress * maxScroll
-
-        imageScrollState.scrollTo(targetScroll.toInt())
-    }
 
     Box(modifier = modifier.fillMaxSize()) {
         if (pages.size == 1) {
@@ -124,7 +114,14 @@ fun StorySlideContent(
             Row(
                 modifier = Modifier
                     .fillMaxSize()
-                    .horizontalScroll(imageScrollState)
+                    .wrapContentWidth(unbounded = true, align = Alignment.Start)
+                    .graphicsLayer {
+                        val progress = (pagerState.currentPage + pagerState.currentPageOffsetFraction) / (pages.size - 1)
+                        val targetScroll = progress * totalScrollableWidth
+
+                        // Move the content visually to the left (negative translation)
+                        translationX = -targetScroll
+                    }
             ) {
                 StoryImage(
                     data = blockItem.image,
@@ -193,36 +190,36 @@ private fun splitTextIntoPages(
 
     val styledText = rememberMarkdownText(text, textStyle, StoryStyleTemplate, textStyle.color)
 
-    val layoutResult = textMeasurer.measure(
-        text = styledText,
-        style = textStyle,
-        constraints = Constraints(maxWidth = maxWidthInPx.toInt()),
-        layoutDirection = layoutDirection,
-    )
+    return remember(styledText, textStyle, screenWidthDp, maxLines) {
+        val layoutResult = textMeasurer.measure(
+            text = styledText,
+            style = textStyle,
+            constraints = Constraints(maxWidth = maxWidthInPx.toInt()),
+            layoutDirection = layoutDirection,
+        )
 
-    val lineCount = layoutResult.lineCount
-    // If line count is less than max lines, return the text as is
-    if (lineCount <= maxLines) {
-        pages.add(styledText)
-        return pages
-    }
-
-    // If line count is more than max lines, split the styledText into pages
-    // Each page will be an AnnotatedString with max 3 line count
-    var currentLine = 0
-    while (currentLine < lineCount) {
-        val start = layoutResult.getLineStart(currentLine)
-        val end = if (currentLine + maxLines < lineCount) {
-            layoutResult.getLineStart(currentLine + maxLines)
+        val lineCount = layoutResult.lineCount
+            // If line count is less than max lines, return the text as is
+        if (lineCount <= maxLines) {
+            pages.add(styledText)
         } else {
-            styledText.length
+            // If line count is more than max lines, split the styledText into pages
+            // Each page will be an AnnotatedString with max 3 line count
+            var currentLine = 0
+            while (currentLine < lineCount) {
+                val start = layoutResult.getLineStart(currentLine)
+                val end = if (currentLine + maxLines < lineCount) {
+                    layoutResult.getLineStart(currentLine + maxLines)
+                } else {
+                    styledText.length
+                }
+                val pageText = styledText.subSequence(start, end)
+                pages.add(pageText)
+                currentLine += maxLines
+            }
         }
-        val pageText = styledText.subSequence(start, end)
-        pages.add(pageText)
-        currentLine += maxLines
+        pages
     }
-
-    return pages
 }
 
 /**
@@ -288,7 +285,9 @@ internal fun StoryImage(
                 width to height
             }
             val availableWidth = (realWidth / realHeight) * screenHeightInPx
-            onAvailableWidth(availableWidth)
+            LaunchedEffect(availableWidth) {
+                onAvailableWidth(availableWidth)
+            }
         }
     }
 
